599404b26da8059eb77f7198cb5371d69e375a53
James Lee and Tyler Ferrin <tyler@>
2016-11-14 13:59:24 -0800

    add the final passing spec for a diagonal attack

--- lib/queen_attack?.rb
+++ lib/queen_attack?.rb
@@ -1,6 +1,8 @@
 class Array
   define_method(:queen_attack?) do |opponent|
-    if self[0] == opponent[0]
+    if self[0] - opponent[0] == self[1] - opponent[1]
+      true
+    elsif  self[0] == opponent[0]
       true
     elsif self[1] == opponent[1]
       true
--- spec/queen_attack?_spec.rb
+++ spec/queen_attack?_spec.rb
@@ -7,11 +7,15 @@ describe("Array#queen_attack?") do
     expect([1,1].queen_attack?([2, 3])).to(eq(false))
   end
 
-  it('is true if the coordinates on the x axis are the same') do
+  it('is true for a horizontal attack') do
     expect([1,1].queen_attack?([1,2])).to(eq(true))
   end
 
-  it('is true if the coordinates on the y axis are the same') do
+  it('is true for a vertical attack') do
     expect([1,1].queen_attack?([2,1])).to(eq(true))
+  end
+
+  it('is true for a diagonal attack') do
+    expect([1,2].queen_attack?([3,4])).to(eq(true))
   end
 end
